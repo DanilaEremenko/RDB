0ce83ae81d26339c3fb84e364750826cc3df23e9
danila <danilaeremenko@mail.ru>
2019-06-16 13:01:00 +0300

    -passing abspath to program-solver added, generator fixed

--- damned_capitalism/managing/algorithms_runer.py
+++ damned_capitalism/managing/algorithms_runer.py
@@ -5,21 +5,23 @@ import os
 
 
 def run_script(alg_dir, interp, exec, max_time):
-    cmd_run = "%s %s/%s -i ../algorithms/items.json -o %s/answer.json" % \
-              (interp, alg_dir, exec, alg_dir,)
-    print("%s" % cmd_run)
+    input_path = os.path.abspath("../algorithms/items.json")
+    output_path = os.path.abspath("%s/answer.json" % alg_dir)
+
+    cmd_run = "%s %s/%s -i %s -o %s" % \
+              (interp, alg_dir, exec, input_path, output_path,)
+    print("%s\n" % cmd_run)
 
     start_time = time.time()
 
-    os.system("rm -f %s/answer.json" % alg_dir)
+    os.system("rm -f %s" % output_path)
     p = subprocess.Popen(cmd_run, stdout=subprocess.PIPE, shell=True)
-    time.sleep(max_time + 0.3)
+    time.sleep(max_time + 0.5)
     p.kill()
 
     exec_time = time.time() - start_time
 
-    print("%s times = %d" % (alg_dir, exec_time))
-
+    print("%s times = %d\n" % (alg_dir, exec_time))
 
 def parse_cfg_of_alg(path_to_cfg):
     alg_dict = json.load(open(path_to_cfg + "/configuration.json", 'r'))
@@ -28,7 +30,7 @@ def parse_cfg_of_alg(path_to_cfg):
 
 
 def insert_result_to_db(cursor, alg_dir, person_id):
-    answer_path = alg_dir + "/answer.json"
+    answer_path = os.path.abspath(alg_dir + "/answer.json")
     no_answer_id = 0
     if os.path.exists(answer_path):
         it_ids = json.load(open(answer_path, 'r'))
@@ -37,12 +39,14 @@ def insert_result_to_db(cursor, alg_dir, person_id):
             cursor.execute(req + "%d);" % it_id)
         os.system("rm %s" % answer_path)
     else:
+        print("no %s for player %s" % (answer_path, alg_dir))
         cursor.execute("insert into person_item values (default ,%d,%d)" % (person_id, no_answer_id))
 
 
 def run_all(cursor, path_to_galg, max_time):
     algs_list = json.load(open(path_to_galg, 'r'))
     person_id = 0
+    print("------------------")
     for alg_dir in algs_list:
         alg_dir = "../algorithms/" + alg_dir
 
@@ -54,3 +58,5 @@ def run_all(cursor, path_to_galg, max_time):
         insert_result_to_db(cursor, alg_dir, person_id)
 
         person_id += 1
+
+        print("------------------")
